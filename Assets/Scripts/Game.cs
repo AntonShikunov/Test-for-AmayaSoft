cead384b9b37982fc97d1705d5050dde9b023f5a
AntonShikunov <lio_polo@bk.ru>
2021-06-08 17:28:24 +0300

    upd

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public Sprite[] _letters = new Sprite[26]; // массив спрайтов с буквами
+    public string[] _ArrLetter = {"A", "B", "C", "D", "F", "G", "J", "I", "E", "H", "K", "L", "M", "R",
+                                    "S", "U", "V", "O", "Q", "W", "T", "N", "P", "X", "Y", "Z"}; // массив всех возможных букв
+    public Image _SpriteLetter; // Image с определённой буквой
+    public static int _level = 1; // номер уровня
+    public static string _arr = ""; 
+    public Text _TaskText;
+    public GameObject _Grid; // сетка, где отображаются и сортируются буквы
+    public GameObject _UI_Window_pref; // префаб окна с буквой
+    public List<string> _PullLetters; // лист всех попавшихся букв в уровне
+
+    public static bool lvlWin = false; // булевое значение показывает, выигран ли уровень
+
+
+    public 
+    void Start()
+    {
+        NextLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _TaskText.text = "Find: " + _arr;
+        if (lvlWin == true)
+        {
+            lvlWin = false;
+            NextLevel();
+        }
+    }
+
+    public void NextLevel()
+    {
+        for (int i = 0; i < (_level * 3); i++)
+        {
+            GameObject pref = Instantiate(_UI_Window_pref, _UI_Window_pref.transform.position, Quaternion.identity) as GameObject;
+            int num_sprite = Random.Range(0, 25);
+            //SpriteLetter.sprite = letters[num_sprite];
+            //SpriteLetter.sprite = letters[num_sprite];
+            pref.GetComponent<Letters>()._SpriteLetter.sprite = _letters[num_sprite];
+            _PullLetters.Add(_ArrLetter[num_sprite]); // добавляется буква в List
+            pref.GetComponent<Letters>().setLetter(_ArrLetter[num_sprite]);
+            pref.transform.SetParent(_Grid.transform);
+            pref.tag = _ArrLetter[num_sprite].ToString();
+            pref.GetComponent<Image>().color = new Color(Random.Range(50, 255) / 255.0f, Random.Range(50, 255) / 255.0f, Random.Range(50, 255) / 255.0f);
+            pref.transform.localScale = new Vector3(1f, 1f, 1f);
+
+
+        }
+        _arr = _PullLetters[Random.Range(0, 3)];
+        _TaskText.text = "Find: " + _arr;
+    }
+    
+
+}
---
+++ Assets/Scripts/Letters.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Letters : MonoBehaviour
+{
+    //public Sprite[] letters = new Sprite[26];
+    public Image _SpriteLetter;
+    private string _letter;
+    public Animation _Letter_fade_in, _ease_in_bounce;
+
+    public Letters (string letter)
+    {
+        this._letter = letter;
+    }
+
+    public string getLetter() { return _letter; }
+    public void setLetter(string let) { _letter = let; }
+
+    void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.950f, 0.950f, 0.950f);
+    }
+    void OnMouseUp()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        clickOnLetter();
+
+    }
+
+    public static void NextLevel() // функция повышения уровня
+    {
+        if (Game._level < 3)
+        {
+            Game.lvlWin = true;
+            Game._level += 1;
+            
+        }
+    }
+
+    public void clickOnLetter()
+    {
+        if (gameObject.tag != Game._arr)
+        {
+            //Game._arr = ".;fffffff";
+            _ease_in_bounce.Play("ease_In_Bounce");
+        }
+        else
+        {
+            Game._arr = "You win! Next lvl...";
+            Game._level += 1;
+            Thread.Sleep(200);
+            NextLevel();
+        }
+    }
+
+
+
+}
